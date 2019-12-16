bbccd2ae189bc5e483469a776b97697028623ad8
Eduardo Enriquez <eduardo.enriquez@wizeline.com>
2019-12-15 21:18:54 -0600

    :fire: project set up

---
+++ backend/todo.go
@@ -0,0 +1,8 @@
+package backend
+
+type Todo struct {
+	ID     string
+	Text   string
+	Done   bool
+	UserID string
+}
---
+++ frontend/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/shurcooL/graphql"
+)
+
+type query struct {
+	Todos []struct {
+		Text graphql.String
+		Done graphql.Boolean
+		User struct {
+			Name graphql.String
+		}
+	}
+}
+
+/*
+	query findTodos {
+		todos {
+		text
+		done
+		user {
+			name
+		}
+		}
+	}
+*/
+func main() {
+	client := graphql.NewClient("http://localhost:8080/query", nil)
+	result := &query{}
+	err := client.Query(context.Background(), result, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(result.Todos[0])
+}
